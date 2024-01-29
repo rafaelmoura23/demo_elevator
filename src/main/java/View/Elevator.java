bff140f075233d73e140aab1d82077cca7577c07
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-01-29 16:16:11 -0300

    Jpanel

--- src/main/java/View/Elevator.java
+++ src/main/java/View/Elevator.java
@@ -1,5 +1,8 @@
 package View;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 public class Elevator {
     // Atributos
 
@@ -7,5 +10,24 @@ public class Elevator {
 
     // JLabel
 
-    // JButton
+    // JButton - floors
+    private JButton floor6;
+    private JButton floor5;
+    private JButton floor4;
+    private JButton floor3;
+    private JButton floor2;
+    private JButton floor1;
+    private JButton floorT;
+    private JButton floorSub1;
+    private JButton floorSub2;
+
+    // Constructor
+    public Elevator() {
+    // JPanel
+        JPanel mainPanel = new JPanel();
+        JPanel elevatorPanel = new JPanel();
+    // Layout
+
+
+    }
 }
