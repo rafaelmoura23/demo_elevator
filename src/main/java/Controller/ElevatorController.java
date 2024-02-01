0d93a86ceee690b043c78867f3664cf81df2d8e9
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-02-01 16:00:22 -0300

    now_ok

---
+++ src/main/java/Controller/ElevatorController.java
@@ -0,0 +1,40 @@
+package Controller;
+
+import Model.Elevador;
+import View.ElevatorGrid;
+
+public class ElevatorController {
+    // private Elevador elevator1;
+    // private Elevador elevator2;
+    // private ElevatorGrid elevatorGrid;
+
+    // public ElevatorController() {
+    //     elevator1 = new Elevador();
+    //     elevator2 = new Elevador();
+    //     elevatorGrid = new ElevatorGrid();
+    // }
+
+    // public void moveElevatorToFloor(int elevatorNumber, int floor) {
+    //     if (elevatorNumber == 1) {
+    //         elevator1.irParaAndar(floor);
+    //         elevatorGrid.updateElevatorStatus(1, floor);
+    //     } else if (elevatorNumber == 2) {
+    //         elevator2.irParaAndar(floor);
+    //         elevatorGrid.updateElevatorStatus(2, floor);
+    //     }
+    // }
+
+    // public void requestFloor(int floor) {
+    //     int closestElevator = calculateElevator(floor);
+    //     moveElevatorToFloor(closestElevator, floor);
+    // }
+
+    // private int calculateElevator(int destinationFloor) {
+    //     int distanceElevator1 = Math.abs(elevator1.getAndarAtual() - destinationFloor);
+    //     int distanceElevator2 = Math.abs(elevator2.getAndarAtual() - destinationFloor);
+
+    //     return (distanceElevator1 < distanceElevator2) ? 1 : 2;
+    // }
+}
+
+
